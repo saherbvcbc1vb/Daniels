5504823e8cbfd2114a5788a6c5acaac5148e7bb0
saherbvcbc1vb <45471374+saherbvcbc1vb@users.noreply.github.com>
2025-10-05 16:09:00 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.querySelector(".bg-nav");
+  const homeSection = document.querySelector("#home");
+
+  const updateNavbar = () => {
+    const homeBottom = homeSection.offsetHeight;
+    if (window.scrollY > homeBottom) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  };
+
+  window.addEventListener("scroll", updateNavbar);
+  updateNavbar();
+});
+var typed = new Typed(".hero-p", {
+  strings: ["Larry Daniels ", "Developer", "Designer"],
+  typeSpeed: 60,
+  loop: true,
+  loopCount: true,
+  backSpeed: 60,
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const counters = document.querySelectorAll(".card-text1");
+
+  const duration = 3000; 
+  
+  const options = {
+    root: null,
+    threshold: 0.5,
+  };
+
+  const startCount = (counter) => {
+    const target = +counter.getAttribute("data-target");
+    let start = 0;
+    const increment = target / (duration / 16); 
+    const update = () => {
+      start += increment;
+      if (start >= target) {
+        counter.textContent = target;
+      } else {
+        counter.textContent = Math.ceil(start);
+        requestAnimationFrame(update);
+      }
+    };
+    
+    update();
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        counters.forEach(counter => startCount(counter));
+        observer.disconnect();
+      }
+    });
+  }, options);
+
+  observer.observe(document.querySelector(".count"));
+});
